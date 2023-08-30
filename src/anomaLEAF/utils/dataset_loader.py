5d62c5008fa32f09f34e16978e0547216ca7d948
rexcrisp <rexcrisp@outlook.com.au>
2023-08-31 09:46:28 +1000

    exception handling for empty directory when trying to add directory to dataset

--- src/anomaLEAF/utils/dataset_loader.py
+++ src/anomaLEAF/utils/dataset_loader.py
@@ -45,7 +45,7 @@ class Dataset():
         additional_dir = Path(additional_dir)
         try: 
             additional_dataset = tf.data.Dataset.list_files(str(additional_dir) + '/*')
-        except ValueError as e:
+        except tf.errors.InvalidArgumentError as e:
             print("Warning: Specified directory was empty and was not added to dataset")
             return
         
