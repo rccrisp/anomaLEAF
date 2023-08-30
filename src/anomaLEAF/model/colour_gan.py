d009d89ccd0fac7bdde1d4acac8475426258f2fb
rexcrisp <rexcrisp@outlook.com.au>
2023-08-30 20:28:59 +1000

    update predict

--- src/anomaLEAF/model/colour_gan.py
+++ src/anomaLEAF/model/colour_gan.py
@@ -283,7 +283,9 @@ class ColourANOM:
 
         anomaly_map = self.anomaly_map(img_rgb, img_reconstructed)
         
-        # colour_map = anomaly_map_to_color_map(anomaly_map)
+        print(type(anomaly_map))
+        print(type(img_rgb))
+        
         heatmap = superimpose_anomaly_map(anomaly_map, img_rgb)
         mask = compute_mask(anomaly_map, 20)
 
