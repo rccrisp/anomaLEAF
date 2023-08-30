d0219d9944a863e80fd2a1592cb161bc09952e77
rexcrisp <rexcrisp@outlook.com.au>
2023-08-31 09:53:39 +1000

    changed add_data_to_dataset function to work for both normal and testing dataset

--- src/anomaLEAF/utils/dataset_loader.py
+++ src/anomaLEAF/utils/dataset_loader.py
@@ -41,12 +41,12 @@ class Dataset():
         self.test_dataset = self.test_dataset.map(self.load_image_test)
         self.test_dataset = self.test_dataset.batch(self.batch_size)
 
-    def add_data_to_normal_dataset(self, additional_dir: str | Path):
+    def add_data_to_dataset(self, additional_dir: str | Path, is_normal: bool):
         additional_dir = Path(additional_dir)
         try: 
             additional_dataset = tf.data.Dataset.list_files(str(additional_dir) + '/*')
         except :
-            print("Warning: Specified directory was empty and was not added to dataset")
+            print("Warning: Specified directory ({additional_dir}) was empty and was not added to dataset")
             return
         
         additional_dataset = additional_dataset.map(self.load_image_train)
@@ -54,8 +54,11 @@ class Dataset():
         # additional_dataset = additional_dataset.shuffle(buffer_size=BUFFER_SIZE)
         additional_dataset = additional_dataset.batch(self.batch_size)
         
-        # Concatenate the additional data to the existing normal_dataset
-        self.normal_dataset = self.normal_dataset.concatenate(additional_dataset)
+        if is_normal:
+            # Concatenate the additional data to the existing normal_dataset
+            self.normal_dataset = self.normal_dataset.concatenate(additional_dataset)
+        else:
+            self.test_dataset = self.test_dataset.concatenate(additional_dataset)
 
     
     def load_image_train(self, file_path):
